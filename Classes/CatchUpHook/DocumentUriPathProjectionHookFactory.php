adcc5e6962873e663312a785b2bcf2797068f6e8
Denny Lubitz <lubitz@vivomedia.de>
2023-10-21 11:03:08 +0200

    TASK: Remove unused registry

--- Classes/CatchUpHook/DocumentUriPathProjectionHookFactory.php
+++ Classes/CatchUpHook/DocumentUriPathProjectionHookFactory.php
@@ -20,7 +20,6 @@ final class DocumentUriPathProjectionHookFactory implements CatchUpHookFactoryIn
     {
         return new DocumentUriPathProjectionHook(
             $contentRepository,
-            $this->contentRepositoryRegistry,
             $this->redirectService
         );
     }
